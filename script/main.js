d5d2f5dd68e18afb7de6ddc5b37d09a7a174e2a5
Amar <imsirovicamar000@gmail.com>
2022-04-23 13:30:56 -0700

    new font and added video player to hero home page

--- script/main.js
+++ script/main.js
@@ -10,6 +10,11 @@ const heroSection = document.getElementById('hero');
 const homeHeroBtn = document.querySelector('.hero-btn');
 const cirlceEl = document.querySelector('.cirlce');
 
+const videoContainer = document.querySelector('.video-container');
+const videoClose = document.querySelector('.video-close');
+
+const videoIframe = document.querySelector('.gm-video');
+
 
 menuOpenBtn.addEventListener('click', () => {
     menuOverlay.classList.add('isOpen');
@@ -29,8 +34,19 @@ menuMask.addEventListener('click', () => {
 homeHeroBtn.addEventListener('mouseover', () => {
     cirlceEl.classList.add('scale')
 })
+
 homeHeroBtn.addEventListener('mouseleave', () => {
     cirlceEl.classList.remove('scale')
+})
+
+homeHeroBtn.addEventListener('click', () => {
+    videoContainer.classList.add('show');
+    videoIframe.src = "https://player.vimeo.com/video/371416332?h=0cebcb48c4&autoplay=1";
+})
+
+videoClose.addEventListener('click', () => {
+    videoContainer.classList.remove('show');
+    videoIframe.src = "";
 })
 
 const firstSectionObserver = new IntersectionObserver((entries) => {
@@ -43,5 +59,7 @@ const firstSectionObserver = new IntersectionObserver((entries) => {
 }, {threshold: 1})
 
 firstSectionObserver.observe(heroSection);
+
+
 
 
